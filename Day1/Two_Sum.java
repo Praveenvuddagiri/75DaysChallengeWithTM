74ea6dbb49f396531baa8362cec1bd97d4ba0b8a
Praveen Vuddagiri <71907901+Praveenvuddagiri@users.noreply.github.com>
2022-04-14 10:08:15 +0530

    Update Two_Sum.java

--- Day1/Two_Sum.java
+++ Day1/Two_Sum.java
@@ -1,13 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int res[] = new int[2];
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if((nums[i]+nums[j])==target){
-                    res[0]=i;
-                    res[1]=j;
-                    break;
-                }
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            int b = target - nums[i];
+            if(map.containsKey(b)){
+                res[1] = i;
+                res[0] = map.get(b);
+                break;
+            }
+            else{
+                map.put(nums[i],i);
             }
         }
         return res;
