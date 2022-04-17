abb52ec76ff829b237a9d111f147eb46b050cfaa
Praveen Vuddagiri <71907901+Praveenvuddagiri@users.noreply.github.com>
2022-04-17 13:10:19 +0530

    Update Remove_Duplicates_from_Sortted_array.java

--- Day3/Remove_Duplicates_from_Sortted_array.java
+++ Day3/Remove_Duplicates_from_Sortted_array.java
@@ -1,7 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int i=1,j=1;
-        int res=1;
         int prev = nums[0];
         while(j<nums.length){
             if(prev == nums[j]){
@@ -9,7 +8,6 @@ class Solution {
             }
             else{
                 prev = nums[j];
-                
                 nums[i]=prev;
                 j++;
                 i++;
